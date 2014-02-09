978b68af0b5a6f81812547a21ae733272f064a9d
XTheDarkX <fampillica@hotmail.com>
2014-02-09 19:40:40 +0100

    Create morpion.c

---
+++ morpion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "morpion.h"
+
+void affichageMorpion() {
+    printf("\n");
+    printf("     1       2       3\n");
+    printf("\n");
+    printf(" * * * * * * * * * * * * *\n");
+    printf(" *       *       *       *\n");
+    printf(" *  X    *   O   *   X   *\n");
+    printf(" *       *       *       *\n");
+    printf(" * * * * * * * * * * * * *\n");
+    printf(" * * * * * * * * * * * * *\n");
+    printf(" *       *       *       *\n");
+    printf(" *  X    *   O   *   X   *\n");
+    printf(" *       *       *       *\n");
+    printf(" * * * * * * * * * * * * *\n");
+    printf(" * * * * * * * * * * * * *\n");
+    printf(" *       *       *       *\n");
+    printf(" *  X    *   O   *   X   *\n");
+    printf(" *       *       *       *\n");
+    printf(" * * * * * * * * * * * * *\n");
+}
