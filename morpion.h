749216dcc42ed113d776dccb946c82baaffcd55d
XTheDarkX <fampillica@hotmail.com>
2014-02-09 20:11:07 +0100

    Update morpion.h

--- morpion.h
+++ morpion.h
@@ -1,13 +1,14 @@
 #ifndef MORPION_H_INCLUDED
 #define MORPION_H_INCLUDED
 
-typedef struct Morpion morpion;
+typedef struct Morpion morp;
 struct Morpion {
     int tableau[3][3];
     int tour;
     int libre;
 };
 
-void affichageMorpion();
+void initialiserTableau(morp *monMorpion);
+void affichageMorpion(morp monMorpion);
 
 #endif // MORPION_H_INCLUDED
