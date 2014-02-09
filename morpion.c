5fbd2395c3786c9278649af4a2a1cc5a12dde07b
XTheDarkX <fampillica@hotmail.com>
2014-02-09 19:56:28 +0100

    Update morpion.c

--- morpion.c
+++ morpion.c
@@ -2,21 +2,31 @@
 #include <stdlib.h>
 #include "morpion.h"
 
-void affichageMorpion() {
+void affichageMorpion(morpion monMorpion) {
+
+    int i = 0, j = 0;
+    char temp[0][0];
+
     printf("\n");
     printf("     1       2       3\n");
     printf("\n");
     printf(" * * * * * * * * * * * * *\n");
     printf(" *       *       *       *\n");
-    printf(" *  %c    *   %c   *   %c   *\n", tableau[0][0], tableau[0][1], tableau[0][2]);
+    printf(" *  %c    *   %c   *   %c   *\n", temp[0][0], temp[0][1], temp[0][2]);
     printf(" *       *       *       *\n");
     printf(" * * * * * * * * * * * * *\n");
     printf(" *       *       *       *\n");
-    printf(" *  %c    *   %c   *   %c   *\n", tableau[1][0], tableau[1][1], tableau[1][2]);
+    printf(" *  %c    *   %c   *   %c   *\n", temp[1][0], temp[1][1], temp[1][2]);
     printf(" *       *       *       *\n");
     printf(" * * * * * * * * * * * * *\n");
     printf(" *       *       *       *\n");
-    printf(" *  %c    *   %c   *   %c   *\n", tableau[2][0], tableau[2][1], tableau[2][2]);
+    printf(" *  %c    *   %c   *   %c   *\n", temp[2][0], temp[2][1], temp[2][2]);
     printf(" *       *       *       *\n");
     printf(" * * * * * * * * * * * * *\n");
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; i < 3; i++) {
+            if()
+        }
+    }
 }
